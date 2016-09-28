5aaec221fc641b4f3fbc650985393c5a9b5b6b0e
webmaxxSELF <webmxxself@163.com>
2016-09-28 23:09:29 +0800

    'update'

---
+++ settings.js
@@ -0,0 +1,10 @@
+/**
+ * 1 如果这个地址会经常改变 大变
+ * 2 在多个模块中引用了这个数据库地址
+ */
+module.exports = {
+    //dbUrl:'mongodb://127.0.0.1:27017/mxxBlog'
+    dbUrl: 'mongodb://localhost/mxxBlog'
+  // dbUrl:'mongodb://123.57.143.189/mxx'
+    //  dbUrl:'mongodb://123.57.143.189/mxx'
+}
